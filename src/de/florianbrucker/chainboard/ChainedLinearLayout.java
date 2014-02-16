cc53265427aa55220377ca48410f31b62b8ec0e7
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-02-16 12:31:44 +0100

    Intercept Motion events at ViewGroup Level
    the buttons are only cosmetical right now. But different Android
    version seem to be inconsistent in their handling of multitouch over
    multiple views, which makes this necessary.

---
+++ src/de/florianbrucker/chainboard/ChainedLinearLayout.java
@@ -0,0 +1,84 @@
+package de.florianbrucker.chainboard;
+
+import java.util.Collection;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class ChainedLinearLayout extends LinearLayout {
+	
+	public interface ChainedTouchEventListener {
+		public void onChainedTouchEvent(int i);
+	}
+	
+	class State {
+		int currentlyDown = 0;
+		int chainLength = 0;
+	}
+	
+	Collection<ChainedTouchEventListener> listeners = new java.util.LinkedList<ChainedTouchEventListener>();
+	
+	public void addListener(ChainedTouchEventListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeListener(ChainedTouchEventListener listener) {
+		listeners.remove(listener);
+	}
+	
+	State state = new State();
+	
+	void fireEvent(int i) {
+		for (ChainedTouchEventListener listener : listeners) {
+			listener.onChainedTouchEvent(i);
+		}
+	}
+	
+	public ChainedLinearLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChainedLinearLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent event){
+		return true;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event){
+		
+		int action = MotionEventCompat.getActionMasked(event);
+		int index = MotionEventCompat.getActionIndex(event);
+		
+		final String DEBUG_TAG = "KEYBOARD";
+		
+		if (action != MotionEvent.ACTION_MOVE){
+			Log.d(DEBUG_TAG,Integer.toString(action) + ","+Integer.toString(index));
+			Log.d(DEBUG_TAG,Integer.toString(state.currentlyDown));
+		}
+		
+		if (action == MotionEvent.ACTION_DOWN || action == MotionEventCompat.ACTION_POINTER_DOWN) {
+			state.currentlyDown++;
+			state.chainLength++;
+		} else if (action == MotionEvent.ACTION_UP || action == MotionEventCompat.ACTION_POINTER_UP) {
+			state.currentlyDown--;
+			if (state.currentlyDown == 0) {
+				fireEvent(state.chainLength);
+				state.chainLength = 0;
+			}
+		}
+		
+		return true;
+		
+	}
+
+}
--- src/de/florianbrucker/chainboard/MainActivity.java
+++ src/de/florianbrucker/chainboard/MainActivity.java
@@ -3,9 +3,8 @@ package de.florianbrucker.chainboard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Button;
 import android.widget.EditText;
-import de.florianbrucker.chainboard.ChainedTouchListener.ChainedTouchEventListener;
+import de.florianbrucker.chainboard.ChainedLinearLayout.ChainedTouchEventListener;
 
 public class MainActivity extends Activity implements ChainedTouchEventListener {
 	
@@ -16,16 +15,8 @@ public class MainActivity extends Activity implements ChainedTouchEventListener 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		ChainedTouchListener touchListener = new ChainedTouchListener();
-		touchListener.addListener(this);
-		
-		((Button) findViewById(R.id.button1)).setOnTouchListener(touchListener);
-		((Button) findViewById(R.id.button2)).setOnTouchListener(touchListener);
-		((Button) findViewById(R.id.button3)).setOnTouchListener(touchListener);
-		((Button) findViewById(R.id.button4)).setOnTouchListener(touchListener);
-		((Button) findViewById(R.id.button5)).setOnTouchListener(touchListener);
-		((Button) findViewById(R.id.button6)).setOnTouchListener(touchListener);
-		
+		ChainedLinearLayout buttongroup = (ChainedLinearLayout) findViewById(R.id.buttongroup);
+		buttongroup.addListener(this);
 	}
 
 	@Override
